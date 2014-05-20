235b3dba892cd479390d8726701d47fbe77c00c7
0x80ff <0x80ff@users.noreply.github.com>
2014-05-20 18:25:30 +0200

    Update PdoConnection.php

--- PdoConnection.php
+++ PdoConnection.php
@@ -4,6 +4,7 @@ try{
 	$strConnection = 'mysql:host=localhost;dbname=stagev2';
 	$arrExtraParam = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 	$pdo           = new PDO($strConnection, 'root', 'root', $arrExtraParam); 
+	
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e)
 {
