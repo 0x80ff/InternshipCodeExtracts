d265676861694dfc67581828cb0e6bbde9c7779c
0x80ff <0x80ff@users.noreply.github.com>
2014-05-20 19:14:40 +0200

    Create PdoPrepare.php

---
+++ PdoPrepare.php
@@ -0,0 +1,20 @@
+<?php
+$numeroAppelé=$numeroDist[$e];
+
+//Requête préparée, insertion données dans la table COMMUNICATIONS
+$requete = 'INSERT INTO communications (abonne_id,dateCommunication,
+                                        numeroDistant,dureeCommunication,
+                                        typeCommunication,etendueCommunication,
+                                        montantCommunication)
+            VALUES (:idabo,:datecom,:numdist,:duration,:typecom,:etend,:mont)';
+            
+$query = $this->db->prepare($requete);
+$query->bindValue(':idabo', $ID, PDO::PARAM_INT);
+$query->bindValue(':datecom', $date[$e], PDO::PARAM_INT);
+$query->bindValue(':numdist', $numeroAppelé, PDO::PARAM_INT);
+$query->bindValue(':duration', $durée[$e], PDO::PARAM_INT);
+$query->bindValue(':typecom', $type, PDO::PARAM_INT);
+$query->bindValue(':etend', $etendue, PDO::PARAM_INT);
+$query->bindValue(':mont', $montant[$e], PDO::PARAM_INT);
+$query->execute();
+?>
