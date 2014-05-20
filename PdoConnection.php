94f840d1a8888f2f4e6da24777a10c180858357d
0x80ff <0x80ff@users.noreply.github.com>
2014-05-20 18:22:41 +0200

    Create PdoConnection.php

---
+++ PdoConnection.php
@@ -0,0 +1,16 @@
+<?php
+
+try
+{
+	$strConnection = 'mysql:host=localhost;dbname=stagev2';
+	$arrExtraParam = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+	$pdo           = new PDO($strConnection, 'root', 'root', $arrExtraParam); 
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+	$msg = 'ERREUR PDO dans ' . $e->getFile() . ' L.' . $e->getLine() . ' : ' . $e->getMessage();
+	die($msg);
+}
+
+?>
